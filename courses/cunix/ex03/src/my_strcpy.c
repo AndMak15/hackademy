936733c21e1e8b4b2c3247283d050932c4e899f4
makalish <makalish.andreas93@gmail.com>
2021-08-25 01:36:04 +0300

    solution ex03

---
+++ courses/cunix/ex03/src/my_strcpy.c
@@ -0,0 +1,10 @@
+char *my_strcpy(char *dest, const char *src) {
+    int index = 0;
+
+    for (;src[index] != '\0'; index++) {
+        dest[index] = src[index];
+    }
+    dest[index] = '\0';
+    
+    return dest;
+}
